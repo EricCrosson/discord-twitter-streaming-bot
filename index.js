1d2d7d855064a6c151bfe2beba16b48b176c3d2b
Eric Crosson <eric.s.crosson@gmail.com>
2017-11-16 03:00:52 -0600

    Format

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ const winston = require('winston')
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.simple(),
-    transports: [
-        new winston.transports.Console()
-    ]
+    transports: [new winston.transports.Console()]
 })
 ////
 
